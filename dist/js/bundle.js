2f94be11c4a20d0b4abcd56b0464cf01d086d4cd
Igor <katsuba96@gmail.com>
2023-04-21 16:49:04 +0300

    first commit

---
+++ dist/js/bundle.js
@@ -0,0 +1,11 @@
+/******/ (() => { // webpackBootstrap
+var __webpack_exports__ = {};
+/*!**************************!*\
+  !*** ./src/js/script.js ***!
+  \**************************/
+window.addEventListener('scroll', function() {
+    document.body.style.cssText += `--scrollTop: ${this.scrollY}px`;
+})
+/******/ })()
+;
+//# sourceMappingURL=bundle.js.map
